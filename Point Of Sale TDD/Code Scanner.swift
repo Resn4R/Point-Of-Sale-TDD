afce5821ce59a82e2691adca8a27b8c4629d1bad
Resn4R <purview-joust.0x@icloud.com>
2023-10-27 16:07:10 +0100

    passed 1 test

---
+++ Point Of Sale TDD/Code Scanner.swift
@@ -0,0 +1,16 @@
+//
+//  Code Scanner.swift
+//  Point Of Sale TDD
+//
+//  Created by Vito Borghi on 27/10/2023.
+//
+
+import Foundation
+class CodeScanner {
+    
+    func scan(barcode: String) -> String {
+        if barcode == "12345" {
+            return "$7.25"
+        } else { return "" }
+    }
+}
