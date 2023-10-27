1aa908a204e11f6323e3d970c24d2227b2708a03
Resn4R <purview-joust.0x@icloud.com>
2023-10-27 15:58:09 +0100

    Initial Commit

---
+++ Point Of Sale TDD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Point Of Sale TDD
+//
+//  Created by Vito Borghi on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
